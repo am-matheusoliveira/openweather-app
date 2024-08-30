e912676eeea62a53ef64aaa549c020a4e152aa38
Matheus Oliveira Pereira <sap.matheusoliveira@gmail.com>
2024-08-30 00:08:10 -0300

    1º commit neste repositório

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}"> {{ config('app.name', 'Laravel') }} </a>
+                <strong><a class="navbar-brand" href="{{ url('/') }}"> {{ config('app.name', 'Laravel') }} </a></strong>
             </div>
         </nav>
 
