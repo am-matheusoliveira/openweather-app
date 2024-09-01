d08c2c7b84a942df56e44914a2853853a2b82ab1
Matheus Oliveira Pereira <sap.matheusoliveira@gmail.com>
2024-08-31 23:07:26 -0300

    1º commit neste repositório

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -14,119 +14,131 @@ use Yajra\DataTables\Facades\DataTables;
 class WeatherController extends Controller
 {
     public function fetchWeatherData(Request $request)
-    {   
-        // TRADUÇÃO DAS CONDIÇÕES CLIMATICAS
-        $translations = [
-            'Clear' => 'Céu limpo',
-            'Clouds' => 'Nuvens',
-            'Rain' => 'Chuva',
-            'Snow' => 'Neve',
-            'Drizzle' => 'Garoa',
-            'Thunderstorm' => 'Trovoada',
-            'Mist' => 'Névoa',
-            'Smoke' => 'Fumaça',
-            'Haze' => 'Neblina',
-            'Dust' => 'Poeira',
-            'Fog' => 'Nevoeiro',
-            'Sand' => 'Areia',
-            'Ash' => 'Cinzas',
-            'Squall' => 'Tempestade',
-            'Tornado' => 'Tornado',
-        ];
+    {
+        // Verifica se os inputs do formulário são nulos
+        if(($request->input('other-city') === null) && ($request->input('select-cidade') === null)){
+            
+            return redirect()->route('weather');
 
-        // CHAVE DE ACESSO A API 
-        $apiKey = '4f3124d762673f41dd1032b718b0ea83';
-        
-        // VARIAVEL QUE RECEBE-RA A RESPOSTA JSON
-        $response = '';
+        }else{
 
-        // SE O USUÁRIO ESCOLHEU UMA CIDADE
-        if($request->filled('select-cidade')){
-            $cityId = $request->input('select-cidade');            
-            $response = Http::get("http://api.openweathermap.org/data/2.5/weather", [
-                'id' => $cityId,
-                'appid' => $apiKey,
-                'units' => 'metric',
-                'lang' => 'pt_br'
-            ]);
-        }
+            // TRADUÇÃO DAS CONDIÇÕES CLIMATICAS
+            $translations = [
+                'Clear' => 'Céu limpo',
+                'Clouds' => 'Nuvens',
+                'Rain' => 'Chuva',
+                'Snow' => 'Neve',
+                'Drizzle' => 'Garoa',
+                'Thunderstorm' => 'Trovoada',
+                'Mist' => 'Névoa',
+                'Smoke' => 'Fumaça',
+                'Haze' => 'Neblina',
+                'Dust' => 'Poeira',
+                'Fog' => 'Nevoeiro',
+                'Sand' => 'Areia',
+                'Ash' => 'Cinzas',
+                'Squall' => 'Tempestade',
+                'Tornado' => 'Tornado',
+            ];
 
-        // SE O USUÁRIO DIGITOU UMA CIDADE
-        if($request->filled('other-city')){
-            $cityName = $request->input('other-city');
-            $response = Http::get("http://api.openweathermap.org/data/2.5/weather", [
-                'q' => $cityName,
-                'appid' => $apiKey,
-                'units' => 'metric',
-                'lang' => 'pt_br'
-            ]);   
-        }
+            // CHAVE DE ACESSO A API 
+            $apiKey = '4f3124d762673f41dd1032b718b0ea83';
 
-        // VERIFICA SE TEVE SUCESSO NA REQUISIÇÃO
-        if ($response->successful()) {
-            $data = $response->json();
+            // VARIAVEL QUE RECEBE-RA A RESPOSTA JSON
+            $response = '';
 
-            // Salvar cidade
-            $city = City::updateOrCreate(
-                ['id' => $data['id']],
-                [
-                    'name'      => $data['name'],
-                    'country'   => $data['sys']['country'],
-                    'longitude' => $data['coord']['lon'],
-                    'latitude'  => $data['coord']['lat']
-                ]
-            );
-
-            // Salvar relatório de clima
-            $report = WeatherReport::create([
-                'city_id'     => $city->id,
-                'timezone'    => $data['timezone'],
-                'temperature' => $data['main']['temp'],
-                'feels_like'  => $data['main']['feels_like'],
-                'temp_min'    => $data['main']['temp_min'],
-                'temp_max'    => $data['main']['temp_max'],
-                'pressure'    => $data['main']['pressure'],
-                'humidity'    => $data['main']['humidity'],
-                'visibility'  => $data['visibility'],
-                'timestamp'   => date('Y-m-d H:i:s', ($data['dt'] + $data['timezone'])),
-                'sunrise'     => date('Y-m-d H:i:s', ($data['sys']['sunrise'] + $data['timezone'])),
-                'sunset'      => date('Y-m-d H:i:s', ($data['sys']['sunset']  + $data['timezone'])),
-            ]);
-
-            // Salvar condições climáticas
-            foreach ($data['weather'] as $key => $condition) {
-                WeatherCondition::create([
-                    'report_id'    => $report->id,
-                    'condition_id' => $condition['id'],
-                    'main'         => $translations[$condition['main']] ?? $condition['main'],
-                    'description'  => $condition['description'],
-                    'icon'         => $condition['icon']
+            // SE O USUÁRIO ESCOLHEU UMA CIDADE
+            if($request->filled('select-cidade')){
+                $cityId = $request->input('select-cidade');
+                $response = Http::get("http://api.openweathermap.org/data/2.5/weather", [
+                    'id' => $cityId,
+                    'appid' => $apiKey,
+                    'units' => 'metric',
+                    'lang' => 'pt_br'
                 ]);
-                
-                // Configurando os campos que seram apresentados na View a partir do JSON da API
-                $data['dt']                    = date('d/m/Y H:i:s', ($data['dt']             + $data['timezone']));
-                $data['sys']['sunset']         = date('d/m/Y H:i:s', ($data['sys']['sunset']  + $data['timezone']));
-                $data['sys']['sunrise']        = date('d/m/Y H:i:s', ($data['sys']['sunrise'] + $data['timezone']));
-                $data['weather'][$key]['main'] = $translations[$condition['main']] ?? $condition['main'];
             }
 
-            // Salvar vento
-            Wind::create([
-                'report_id' => $report->id,
-                'speed'     => $data['wind']['speed'],
-                'direction' => $data['wind']['deg']
-            ]);
+            // SE O USUÁRIO DIGITOU UMA CIDADE
+            if($request->filled('other-city')){
+                $cityName = $request->input('other-city');
+                $response = Http::get("http://api.openweathermap.org/data/2.5/weather", [
+                    'q' => $cityName,
+                    'appid' => $apiKey,
+                    'units' => 'metric',
+                    'lang' => 'pt_br'
+                ]);   
+            }
+            
+            // Limpando os inputs vindo do formulario
+            $request->input('other-city', null);
+            $request->input('select-cidade', null);
 
-            // Salvar nuvens
-            Cloud::create([
-                'report_id'  => $report->id,
-                'cloudiness' => $data['clouds']['all']
-            ]);
+            // VERIFICA SE TEVE SUCESSO NA REQUISIÇÃO
+            if ($response->successful()) {
+                $data = $response->json();
 
-            return redirect()->route('weather')->with(['messageSuccess' => 'Dados salvos com sucesso!', 'weatherData' => $data]);            
+                // Salvar cidade
+                $city = City::updateOrCreate(
+                    ['id' => $data['id']],
+                    [
+                        'name'      => $data['name'],
+                        'country'   => $data['sys']['country'],
+                        'longitude' => $data['coord']['lon'],
+                        'latitude'  => $data['coord']['lat']
+                    ]
+                );
+
+                // Salvar relatório de clima
+                $report = WeatherReport::create([
+                    'city_id'     => $city->id,
+                    'timezone'    => $data['timezone'],
+                    'temperature' => $data['main']['temp'],
+                    'feels_like'  => $data['main']['feels_like'],
+                    'temp_min'    => $data['main']['temp_min'],
+                    'temp_max'    => $data['main']['temp_max'],
+                    'pressure'    => $data['main']['pressure'],
+                    'humidity'    => $data['main']['humidity'],
+                    'visibility'  => $data['visibility'],
+                    'timestamp'   => date('Y-m-d H:i:s', ($data['dt'] + $data['timezone'])),
+                    'sunrise'     => date('Y-m-d H:i:s', ($data['sys']['sunrise'] + $data['timezone'])),
+                    'sunset'      => date('Y-m-d H:i:s', ($data['sys']['sunset']  + $data['timezone'])),
+                ]);
+
+                // Salvar condições climáticas
+                foreach ($data['weather'] as $key => $condition) {
+                    WeatherCondition::create([
+                        'report_id'    => $report->id,
+                        'condition_id' => $condition['id'],
+                        'main'         => $translations[$condition['main']] ?? $condition['main'],
+                        'description'  => $condition['description'],
+                        'icon'         => $condition['icon']
+                    ]);
+
+                    // Configurando os campos que seram apresentados na View a partir do JSON da API
+                    $data['dt']                    = date('d/m/Y H:i:s', ($data['dt']             + $data['timezone']));
+                    $data['sys']['sunset']         = date('d/m/Y H:i:s', ($data['sys']['sunset']  + $data['timezone']));
+                    $data['sys']['sunrise']        = date('d/m/Y H:i:s', ($data['sys']['sunrise'] + $data['timezone']));
+                    $data['weather'][$key]['main'] = $translations[$condition['main']] ?? $condition['main'];
+                }
+
+                // Salvar vento
+                Wind::create([
+                    'report_id' => $report->id,
+                    'speed'     => $data['wind']['speed'],
+                    'direction' => $data['wind']['deg']
+                ]);
+
+                // Salvar nuvens
+                Cloud::create([
+                    'report_id'  => $report->id,
+                    'cloudiness' => $data['clouds']['all']
+                ]);
+
+                return redirect()->route('weather')->with(['messageSuccess' => 'Dados salvos com sucesso!', 'weatherData' => $data]);            
+            }
+
+            return redirect()->route('weather')->with(['messageDanger' => 'Erro ao consultar a API.']);
         }
-        
-        return redirect()->route('weather')->with(['messageDanger' => 'Erro ao consultar a API.']);
     }
     
     // Retorna para a view de listagem dos Dados climáticas
